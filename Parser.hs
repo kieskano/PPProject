d377265a95ae776f09bfb7529ab9fd3738bba5e7
Han Hollander <han_hollander@hotmail.nl>
2017-06-15 21:00:09 +0200

    parse to ast

--- Parser.hs
+++ Parser.hs
@@ -4,6 +4,10 @@ import Tokenizer
 import Grammar
 import ParseBasis
 import ParserGen
+import FPPrac.Trees
 
 parseDinkie :: String -> ParseTree
 parseDinkie file = parse grammar Prog $ lexer $ tokenize $ getFileString file
+
+
+test = showRoseTree (toRoseTree (parseDinkie "testSmall.ding"))
