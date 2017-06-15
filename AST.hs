5af17a7c2c758c4e842dfb0389963b79cc113140
Wijtse Rekker <wijtse@rekkerit.nl>
2017-06-15 23:59:19 +0200

    finished 'correct operation order in expressions' function

--- AST.hs
+++ AST.hs
@@ -1,6 +1,7 @@
 module AST where
 
 import FPPrac.Trees
+import ParseBasis
 
 -- ======================================================================================= --
 -- ================================= AST definition ====================================== --
@@ -21,51 +22,19 @@ data AST    = ProgT [AST]
             | VarT String
             | OneOpT String AST
             | TwoOpT AST String AST
-
-
-
-parsetoast :: ParseTree -> AST
-parsetoast (PNode Prog stats)                       = ProgT (map parsetoast stats)
--- Statements
-parsetoast (PNode Stat [PNode Decl [t, n]])     = Decl (getTokenString t) (getTokenString n) EmptyT
-parsetoast (PNode Stat [PNode Decl [t, n, e]])  = Decl (getTokenString t) (getTokenString n) (parsetoast e)
-parsetoast (PNode Stat [PNode Assign [n, e]])   = Assign (getTokenString n) (parsetoast e)
-parsetoast (PNode Stat [Pnode While [e, s]])    = WhileT (parsetoast e) (map parsetoast s)
-parsetoast (PNode Stat [Pnode IfOne [e, s]])    = IfOneT (parsetoast e) (map parsetoast s)
-parsetoast (PNode Stat [Pnode IfTwo [e, st, se]]) = IfTwoT (parsetoast e) (map parsetoast st) (map parsetoast se)
--- Expressions
-parsetoast (PNode Expr [Val v, TwoOp t, Expr e]) = TwoOpT (getTokenString t (parsetoast (Val v)) (parsetoast (Expr e))
-parsetoast (PNode Expr [Expr e])                = parsetoast (Expr e)
-parsetoast (PNode Expr [OneOp o, Expr e])       = OneOpT (getTokenString o) (parsetoast (Expr e))
-parsetoast (PNode Expr [Val [IntConst i]])      = IntConstT (getTokenString i)
-parsetoast (PNode Expr [Val [BoolConst b]])     = BoolConstT (getTokenString b)
-parsetoast (PNode Expr [Val [Var v]])           = VarT (getTokenString v)
-
-
-
-
-
-
-getTokenString :: ParseTree -> String
-getTokenString pt   = case pt of
-    PLeaf (a, s)-> s
-    otherwise   -> error "IN getTokenString : is not a leaf"
-
-
-
+            | BracketsT AST -- Gone after expression correction
+            deriving (Show, Eq)
 
 
 
 asttorose :: AST -> RoseTree
 asttorose (ProgT asts)              = RoseNode "ProgT" (map asttorose asts)
--- Statements
 asttorose (DeclT s1 s2 ast)         = RoseNode ("DeclT " ++ s1 ++ " " ++ s2) [asttorose ast]
 asttorose (AssignT s ast)           = RoseNode ("AssignT " ++ s) [asttorose ast]
 asttorose (WhileT ast asts)         = RoseNode "WhileT" ((asttorose ast):(map asttorose asts))
 asttorose (IfOneT ast asts)         = RoseNode "IfOneT" ((asttorose ast):(map asttorose asts))
 asttorose (IfTwoT ast asts1 asts2)  = RoseNode "IfTwoT" (((asttorose ast):(map asttorose asts1)) ++ (map asttorose asts2))
 asttorose EmptyT                    = RoseNode "Empty" []
--- Expressions
 asttorose (IntConstT x)             = RoseNode ("IntConstT " ++ show(x)) []
 asttorose (BoolConstT b)            = RoseNode ("BoolConstT " ++ show(b)) []
 asttorose (VarT s)                  = RoseNode ("VarT " ++ s) []
--- CorrectAST.hs
+++ CorrectAST.hs
@@ -1,4 +1,37 @@
 import AST
+import Data.List
+
+-- ======================================================================================= --
+-- ========================== Operator ordering definition =============================== --
+-- ======================================================================================= --
+
+op2OrderingMap :: [(String, Int)]
+op2OrderingMap =   [("+", 0),
+                    ("-", 0),
+                    ("*", 1),
+                    ("%", 1),
+                    ("==", 2),
+                    ("!=", 2),
+                    ("<", 2),
+                    (">", 2),
+                    ("<=", 2),
+                    (">=", 2),
+                    ("||", 3),
+                    ("&&", 3),
+                    ("+|", 3)]
+
+op2Ordering :: String -> Int
+op2Ordering op = case (lookup op op2OrderingMap) of
+                    Just x      -> x
+                    _           -> error ("IN op2Ordering : operator ordering " ++ (show op) ++ " not defined!")
+
+op2OrdCompare :: ExprString -> ExprString -> Ordering
+op2OrdCompare (Operator2 s1) (Operator2 s2) | op1Ord < op2Ord   = LT
+                                            | op1Ord > op2Ord   = GT
+                                            | otherwise         = EQ
+                                            where
+                                                op1Ord = op2Ordering s1
+                                                op2Ord = op2Ordering s2
 
 correctProg :: AST -> AST
 correctProg (ProgT asts)            = ProgT (map correctProg asts)
@@ -13,20 +46,42 @@ correctProg (EmptyT)                = EmptyT
 data ExprString = Integer Int
                 | Boolean Bool
                 | Variable String
-                | Operator String
+                | Brackets AST
+                | Operator1 String
+                | Operator2 String
+                deriving (Show, Eq)
 
 correctExpr :: AST -> AST
-correctExpr ast =
+correctExpr ast = listToExpr exprStrings operators'
                 where
                     exprStrings = exprToList ast
-                    operators = filter isOperator exprStrings
+                    operators = filter isOperator2 exprStrings
+                    operators' = sortBy op2OrdCompare operators
 
 exprToList :: AST -> [ExprString]
 exprToList (IntConstT x)    = [Integer x]
 exprToList (BoolConstT x)   = [Boolean x]
 exprToList (VarT x)         = [Variable x]
+exprToList (BracketsT a)    = [Brackets (correctExpr a)]
+exprToList (OneOpT s a)     = [Operator1 s] ++ (exprToList a)
 exprToList (TwoOpT a1 s a2) = (exprToList a1) ++ [Operator2 s] ++ (exprToList a2)
 
-isOperator :: ExprString -> Bool
-isOperator (Operator _) = True
-isOperator _            = False
+listToExpr :: [ExprString] -> [ExprString] -> AST
+listToExpr [Operator1 s, x] ys      = OneOpT s (listToExpr [x] ys)
+listToExpr [Integer x] ys           = IntConstT x
+listToExpr [Boolean x] ys           = BoolConstT x
+listToExpr [Variable x] ys          = VarT x
+listToExpr [Brackets x] ys          = x
+listToExpr xs ((Operator2 op):ys)   | elem (Operator2 op) xs    = TwoOpT (listToExpr rxs ys) op (listToExpr lxs ys)
+                                    | otherwise                 = listToExpr xs ys
+                                    where
+                                        (rxs, lxs) = splitListOn (Operator2 op) xs
+
+isOperator2 :: ExprString -> Bool
+isOperator2 (Operator2 _) = True
+isOperator2 _             = False
+
+splitListOn :: Eq a => a -> [a] -> ([a],[a])
+splitListOn x []                 = ([], [])
+splitListOn x (y:ys) | x == y    = ([], ys)
+                     | otherwise = let (a, b) = splitListOn x ys in (y:a, b)
