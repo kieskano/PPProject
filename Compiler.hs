322d4656f1eea061b713023745574d5e872d6632
Han Hollander <han_hollander@hotmail.nl>
2017-06-20 20:14:55 +0200

    . not -

--- Compiler.hs
+++ Compiler.hs
@@ -17,6 +17,7 @@ test1 = showRoseTree (toRoseTree (parseDinkie "testSmall.ding"))
 test = showRoseTree (asttorose (parsetoast (parseDinkie "testScope2.ding")))
 test2 = checkScope (parsetoast (parseDinkie "testScope2.ding"))
 test3 = compileDinkie "testScope2.ding"
+test4 = compileDinkie "testType.ding"
 
 compileDinkie :: String -> [String]
 compileDinkie file  | length scopeErrors /= 0   = error $ ('\n':) $ unlines scopeErrors
