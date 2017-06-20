8738067e87e49e9a8491f31e841d22414dccb86b
Han Hollander <han_hollander@hotmail.nl>
2017-06-20 11:34:31 +0200

    help w halp

--- AST.hs
+++ AST.hs
@@ -17,8 +17,8 @@ data AST    = ProgT [AST]
             | IfTwoT AST [AST] [AST]
             | EmptyT
             -- Expressions
-            | IntConstT Int
-            | BoolConstT Bool
+            | IntConstT String
+            | BoolConstT String
             | VarT String
             | OneOpT String AST
             | TwoOpT AST String AST
@@ -33,15 +33,17 @@ parsetoast (PNode Stat [PNode Decl [t, n, e]])      = DeclT (getTokenString t) (
 parsetoast (PNode Stat [PNode Assign [n, e]])       = AssignT (getTokenString n) (parsetoast e)
 parsetoast (PNode Stat [PNode While [e, PNode Block s]])                    = WhileT (parsetoast e) (map parsetoast s)
 parsetoast (PNode Stat [PNode IfOne [e, PNode Block s]])                    = IfOneT (parsetoast e) (map parsetoast s)
-parsetoast (PNode Stat [PNode IfTwo [e, PNode Block st, PNode Block se]])   = IfTwoT (parsetoast e) (parsetoast st) (parsetoast se)
+parsetoast (PNode Stat [PNode IfTwo [e, PNode Block st, PNode Block se]])   = IfTwoT (parsetoast e) (map parsetoast st) (map parsetoast se)
 -- Expressions
-parsetoast (PNode Expr [Val v, TwoOp t, Expr e])    = TwoOpT (getTokenString t (parsetoast (Val v)) (parsetoast (Expr e)))
-parsetoast (PNode Expr [Expr e])                    = parsetoast (Expr e)
-parsetoast (PNode Expr [OneOp o, Expr e])           = OneOpT (getTokenString o) (parsetoast (Expr e))
-parsetoast (PNode Expr [Brackets [Expr e]])         = BracketsT (parsetoast(Expr e))
-parsetoast (PNode Expr [Val [IntConst i]])          = IntConstT (getTokenString i)
-parsetoast (PNode Expr [Val [BoolConst b]])         = BoolConstT (getTokenString b)
-parsetoast (PNode Expr [Val [Var v]])               = VarT (getTokenString v)
+parsetoast (PNode Expr [PNode Val [v], PNode TwoOp [t], PNode Expr [e]])    = TwoOpT (parsetoast v) (getTokenString (t)) (parsetoast e)
+parsetoast (PNode Expr [PNode Expr e])                                      = parsetoast (PNode Expr e)
+parsetoast (PNode Expr [PNode OneOp [o], PNode Expr e])                     = OneOpT (getTokenString o) (parsetoast (PNode Expr e))
+parsetoast (PNode Expr [PNode Brackets [PNode Expr e]])                     = BracketsT (parsetoast(PNode Expr e))
+parsetoast (PNode Expr [PNode Val [PNode IntConst [i]]])                    = IntConstT (getTokenString i)
+parsetoast (PNode Expr [PNode Val [PNode BoolConst [b]]])                   = BoolConstT (getTokenString b)
+parsetoast (PNode Expr [PNode Val [PNode Var [v]]])                         = VarT (getTokenString v)
+-- Rest
+parsetoast x = error ("Error in parsetoast on: " ++ show(x))
 
 
 asttorose :: AST -> RoseTree
@@ -54,8 +56,8 @@ asttorose (IfOneT ast asts)         = RoseNode "IfOneT" ((asttorose ast):(map as
 asttorose (IfTwoT ast asts1 asts2)  = RoseNode "IfTwoT" (((asttorose ast):(map asttorose asts1)) ++ (map asttorose asts2))
 asttorose EmptyT                    = RoseNode "Empty" []
 -- Expressions
-asttorose (IntConstT x)             = RoseNode ("IntConstT " ++ show(x)) []
-asttorose (BoolConstT b)            = RoseNode ("BoolConstT " ++ show(b)) []
+asttorose (IntConstT x)             = RoseNode ("IntConstT " ++ x) []
+asttorose (BoolConstT b)            = RoseNode ("BoolConstT " ++ b) []
 asttorose (VarT s)                  = RoseNode ("VarT " ++ s) []
 asttorose (OneOpT s ast)            = RoseNode ("OneOpT " ++ s) [asttorose ast]
 asttorose (TwoOpT ast1 s ast2)      = RoseNode ("TwoOpT " ++ s) ((asttorose ast1):[asttorose ast2])
@@ -72,19 +74,19 @@ getTokenString pt   = case pt of
 
 
 tAST = ProgT [
-        DeclT "#" "a" (TwoOpT (OneOpT "-"  (IntConstT 2)) "==" (VarT "b")),
+        DeclT "#" "a" (TwoOpT (OneOpT "-"  (IntConstT "2")) "==" (VarT "b")),
         DeclT "?" "b" EmptyT,
-        AssignT "b" (BoolConstT False),
+        AssignT "b" (BoolConstT "\\"),
         WhileT (VarT "b") [
-            AssignT "b" (BoolConstT False)
+            AssignT "b" (BoolConstT "\\")
         ],
         IfOneT (VarT "b") [
-            AssignT "d" (BoolConstT False)
+            AssignT "d" (BoolConstT "\\")
         ],
         IfTwoT (VarT "e") [
-            AssignT "f" (BoolConstT False)
+            AssignT "f" (BoolConstT "\\")
         ] [
-            AssignT "c" (BoolConstT False)
+            AssignT "c" (BoolConstT "\\")
         ]
     ]
 
