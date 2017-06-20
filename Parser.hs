4c9749b5f7c2565dcc47dc47f205f524754e9b1c
Han Hollander <han_hollander@hotmail.nl>
2017-06-20 16:26:57 +0200

    almost checkuse

--- Parser.hs
+++ Parser.hs
@@ -12,5 +12,5 @@ parseDinkie :: String -> ParseTree
 parseDinkie file = parse grammar Prog $ lexer $ tokenize $ getFileString file
 
 test1 = showRoseTree (toRoseTree (parseDinkie "testSmall.ding"))
-test = showRoseTree (asttorose (parsetoast (parseDinkie "test.ding")))
-test2 = checkScope' (parsetoast (parseDinkie "testScope.ding")) []
+test = showRoseTree (asttorose (parsetoast (parseDinkie "testScope2.ding")))
+test2 = checkScope (parsetoast (parseDinkie "testScope2.ding"))
