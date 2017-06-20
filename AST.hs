3ead7d8222cac3a2db8c5417fd352f63dc6a6ff3
Han Hollander <han_hollander@hotmail.nl>
2017-06-20 11:51:20 +0200

    ast works

--- AST.hs
+++ AST.hs
@@ -35,13 +35,16 @@ parsetoast (PNode Stat [PNode While [e, PNode Block s]])                    = Wh
 parsetoast (PNode Stat [PNode IfOne [e, PNode Block s]])                    = IfOneT (parsetoast e) (map parsetoast s)
 parsetoast (PNode Stat [PNode IfTwo [e, PNode Block st, PNode Block se]])   = IfTwoT (parsetoast e) (map parsetoast st) (map parsetoast se)
 -- Expressions
-parsetoast (PNode Expr [PNode Val [v], PNode TwoOp [t], PNode Expr [e]])    = TwoOpT (parsetoast v) (getTokenString (t)) (parsetoast e)
+parsetoast (PNode Expr [v, PNode TwoOp [t], e])                             = TwoOpT (parsetoast v) (getTokenString (t)) (parsetoast e)
 parsetoast (PNode Expr [PNode Expr e])                                      = parsetoast (PNode Expr e)
 parsetoast (PNode Expr [PNode OneOp [o], PNode Expr e])                     = OneOpT (getTokenString o) (parsetoast (PNode Expr e))
 parsetoast (PNode Expr [PNode Brackets [PNode Expr e]])                     = BracketsT (parsetoast(PNode Expr e))
 parsetoast (PNode Expr [PNode Val [PNode IntConst [i]]])                    = IntConstT (getTokenString i)
 parsetoast (PNode Expr [PNode Val [PNode BoolConst [b]]])                   = BoolConstT (getTokenString b)
 parsetoast (PNode Expr [PNode Val [PNode Var [v]]])                         = VarT (getTokenString v)
+parsetoast (PNode Val [PNode IntConst [i]])                                 = IntConstT (getTokenString i)
+parsetoast (PNode Val [PNode BoolConst [b]])                                = BoolConstT (getTokenString b)
+parsetoast (PNode Val [PNode Var [v]])                                      = VarT (getTokenString v)
 -- Rest
 parsetoast x = error ("Error in parsetoast on: " ++ show(x))
 
@@ -54,13 +57,15 @@ asttorose (AssignT s ast)           = RoseNode ("AssignT " ++ s) [asttorose ast]
 asttorose (WhileT ast asts)         = RoseNode "WhileT" ((asttorose ast):(map asttorose asts))
 asttorose (IfOneT ast asts)         = RoseNode "IfOneT" ((asttorose ast):(map asttorose asts))
 asttorose (IfTwoT ast asts1 asts2)  = RoseNode "IfTwoT" (((asttorose ast):(map asttorose asts1)) ++ (map asttorose asts2))
-asttorose EmptyT                    = RoseNode "Empty" []
+asttorose EmptyT                    = RoseNode "EmptyT" []
+asttorose (BracketsT ast)           = RoseNode "BracketsT" [(asttorose ast)]
 -- Expressions
 asttorose (IntConstT x)             = RoseNode ("IntConstT " ++ x) []
 asttorose (BoolConstT b)            = RoseNode ("BoolConstT " ++ b) []
 asttorose (VarT s)                  = RoseNode ("VarT " ++ s) []
 asttorose (OneOpT s ast)            = RoseNode ("OneOpT " ++ s) [asttorose ast]
 asttorose (TwoOpT ast1 s ast2)      = RoseNode ("TwoOpT " ++ s) ((asttorose ast1):[asttorose ast2])
+-- asttorose x                         = error ("Error in asttorose on: " ++ show(x))
 
 
 
