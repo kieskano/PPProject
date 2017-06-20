0f58ad33c2e2f86fc9ccf6d604d5cef3cce16de4
Wijtse Rekker <wijtse@rekkerit.nl>
2017-06-20 10:25:35 +0200

    parsetoast is er weer

--- AST.hs
+++ AST.hs
@@ -25,6 +25,22 @@ data AST    = ProgT [AST]
             | BracketsT AST -- Gone after expression correction
             deriving (Show, Eq)
 
+parsetoast :: ParseTree -> AST
+parsetoast (PNode Prog stats)                       = ProgT (map parsetoast stats)
+-- Statements
+parsetoast (PNode Stat [PNode Decl [t, n]])     = Decl (getTokenString t) (getTokenString n) EmptyT
+parsetoast (PNode Stat [PNode Decl [t, n, e]])  = Decl (getTokenString t) (getTokenString n) (parsetoast e)
+parsetoast (PNode Stat [PNode Assign [n, e]])   = Assign (getTokenString n) (parsetoast e)
+parsetoast (PNode Stat [Pnode While [e, s]])    = WhileT (parsetoast e) (map parsetoast s)
+parsetoast (PNode Stat [Pnode IfOne [e, s]])    = IfOneT (parsetoast e) (map parsetoast s)
+parsetoast (PNode Stat [Pnode IfTwo [e, st, se]]) = IfTwoT (parsetoast e) (map parsetoast st) (map parsetoast se)
+-- Expressions
+parsetoast (PNode Expr [Val v, TwoOp t, Expr e]) = TwoOpT (getTokenString t (parsetoast (Val v)) (parsetoast (Expr e))
+parsetoast (PNode Expr [Expr e])                = parsetoast (Expr e)
+parsetoast (PNode Expr [OneOp o, Expr e])       = OneOpT (getTokenString o) (parsetoast (Expr e))
+parsetoast (PNode Expr [Val [IntConst i]])      = IntConstT (getTokenString i)
+parsetoast (PNode Expr [Val [BoolConst b]])     = BoolConstT (getTokenString b)
+parsetoast (PNode Expr [Val [Var v]])           = VarT (getTokenString v)
 
 
 asttorose :: AST -> RoseTree
