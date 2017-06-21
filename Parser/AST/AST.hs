0da69370a2a19818ea75517a623a4a632e07773d
Wijtse Rekker <wijtse@rekkerit.nl>
2017-06-21 11:35:49 +0200

    Changed AST to fit the new syntax

--- Parser/AST/AST.hs
+++ Parser/AST/AST.hs
@@ -10,16 +10,19 @@ import Parser.ParseBasis
 
 data AST    = ProgT [AST]
             -- Statements
-            | DeclT String String AST
+            | GlobalDeclT String String AST
+            | PrivateDeclT String String AST
             | AssignT String AST
             | WhileT AST [AST]
             | IfOneT AST [AST]
             | IfTwoT AST [AST] [AST]
+            | ParallelT AST [AST]
             | EmptyT
             -- Expressions
             | IntConstT String
             | BoolConstT String
             | VarT String
+            | ThreadIDT
             | OneOpT String AST
             | TwoOpT AST String AST
             | BracketsT AST -- Gone after expression correction
@@ -28,12 +31,15 @@ data AST    = ProgT [AST]
 parsetoast :: ParseTree -> AST
 parsetoast (PNode Prog stats)                       = ProgT (map parsetoast stats)
 -- Statements
-parsetoast (PNode Stat [PNode Decl [t, n]])         = DeclT (getTokenString t) (getTokenString n) EmptyT
-parsetoast (PNode Stat [PNode Decl [t, n, e]])      = DeclT (getTokenString t) (getTokenString n) (parsetoast e)
+parsetoast (PNode Stat [PNode Decl [t, n]])         = PrivateDeclT (getTokenString t) (getTokenString n) EmptyT
+parsetoast (PNode Stat [PNode Decl [t, n, e]])      = PrivateDeclT (getTokenString t) (getTokenString n) (parsetoast e)
+parsetoast (PNode Stat [_, PNode Decl [t, n]])      = GlobalDeclT (getTokenString t) (getTokenString n) EmptyT
+parsetoast (PNode Stat [_, PNode Decl [t, n, e]])   = GlobalDeclT (getTokenString t) (getTokenString n) (parsetoast e)
 parsetoast (PNode Stat [PNode Assign [n, e]])       = AssignT (getTokenString n) (parsetoast e)
 parsetoast (PNode Stat [PNode While [e, PNode Block s]])                    = WhileT (parsetoast e) (map parsetoast s)
 parsetoast (PNode Stat [PNode IfOne [e, PNode Block s]])                    = IfOneT (parsetoast e) (map parsetoast s)
 parsetoast (PNode Stat [PNode IfTwo [e, PNode Block st, PNode Block se]])   = IfTwoT (parsetoast e) (map parsetoast st) (map parsetoast se)
+parsetoast (PNode Stat [PNode Parallel [PNode IntConst [i], PNode Block st]])   = ParallelT (IntConstT (getTokenString i)) (map parsetoast st)
 -- Expressions
 parsetoast (PNode Expr [PNode Brackets [v], PNode TwoOp [t], e])            = TwoOpT (parsetoast (PNode Expr [PNode Brackets [v]])) (getTokenString (t)) (parsetoast(e))
 parsetoast (PNode Expr [v, PNode TwoOp [t], e])                             = TwoOpT (parsetoast v) (getTokenString (t)) (parsetoast e)
@@ -46,6 +52,7 @@ parsetoast (PNode Expr [PNode Val [PNode Var [v]]])                         = Va
 parsetoast (PNode Val [PNode IntConst [i]])                                 = IntConstT (getTokenString i)
 parsetoast (PNode Val [PNode BoolConst [b]])                                = BoolConstT (getTokenString b)
 parsetoast (PNode Val [PNode Var [v]])                                      = VarT (getTokenString v)
+parsetoast (PNode Val [PNode ThreadID []])                                  = ThreadIDT
 -- Rest
 parsetoast x = error ("Error in parsetoast on: " ++ show(x))
 
@@ -53,17 +60,20 @@ parsetoast x = error ("Error in parsetoast on: " ++ show(x))
 asttorose :: AST -> RoseTree
 asttorose (ProgT asts)              = RoseNode "ProgT" (map asttorose asts)
 --
-asttorose (DeclT s1 s2 ast)         = RoseNode ("DeclT " ++ s1 ++ " " ++ s2) [asttorose ast]
+asttorose (GlobalDeclT s1 s2 ast)   = RoseNode ("DeclT _" ++ s1 ++ " " ++ s2) [asttorose ast]
+asttorose (PrivateDeclT s1 s2 ast)  = RoseNode ("DeclT " ++ s1 ++ " " ++ s2) [asttorose ast]
 asttorose (AssignT s ast)           = RoseNode ("AssignT " ++ s) [asttorose ast]
 asttorose (WhileT ast asts)         = RoseNode "WhileT" ((asttorose ast):(map asttorose asts))
 asttorose (IfOneT ast asts)         = RoseNode "IfOneT" ((asttorose ast):(map asttorose asts))
 asttorose (IfTwoT ast asts1 asts2)  = RoseNode "IfTwoT" (((asttorose ast):(map asttorose asts1)) ++ (map asttorose asts2))
+asttorose (ParallelT ast asts)      = RoseNode "ParallelT" ((asttorose ast):(map asttorose asts))
 asttorose EmptyT                    = RoseNode "EmptyT" []
 asttorose (BracketsT ast)           = RoseNode "BracketsT" [(asttorose ast)]
 -- Expressions
 asttorose (IntConstT x)             = RoseNode ("IntConstT " ++ x) []
 asttorose (BoolConstT b)            = RoseNode ("BoolConstT " ++ b) []
 asttorose (VarT s)                  = RoseNode ("VarT " ++ s) []
+asttorose ThreadIDT                 = RoseNode ("ThreadIDT") []
 asttorose (OneOpT s ast)            = RoseNode ("OneOpT " ++ s) [asttorose ast]
 asttorose (TwoOpT ast1 s ast2)      = RoseNode ("TwoOpT " ++ s) ((asttorose ast1):[asttorose ast2])
 -- asttorose x                         = error ("Error in asttorose on: " ++ show(x))
@@ -80,8 +90,8 @@ getTokenString pt   = case pt of
 
 
 tAST = ProgT [
-        DeclT "#" "a" (TwoOpT (OneOpT "-"  (IntConstT "2")) "==" (VarT "b")),
-        DeclT "?" "b" EmptyT,
+        PrivateDeclT "#" "a" (TwoOpT (OneOpT "-"  (IntConstT "2")) "==" (VarT "b")),
+        PrivateDeclT "?" "b" EmptyT,
         AssignT "b" (BoolConstT "\\"),
         WhileT (VarT "b") [
             AssignT "b" (BoolConstT "\\")
--- WijtsesTestFile.hs
+++ WijtsesTestFile.hs
@@ -1,17 +1,17 @@
-import Compiler (parseDinkie)
-import Parser.AST.CorrectAST (correctProg)
+-- import Compiler (parseDinkie)
+-- import Parser.AST.CorrectAST (correctProg)
 import Parser.AST.AST
-import Checker.TypeChecker (checkTypes)
-import FPPrac.Trees
+-- import Checker.TypeChecker (checkTypes)
+-- import FPPrac.Trees
 
-compileDinkie :: String -> [String]
-compileDinkie file  | length typeErrors /= 0    = error $ ('\n':) $ unlines typeErrors
-                    | otherwise                 = []
-                    where
-                        parseTree = parseDinkie file
-                        ast = parsetoast parseTree
-                        ast' = correctProg ast
-                        typeErrors = snd $ checkTypes [] ast'
-
-test1 = showRoseTree $ asttorose $ parsetoast $ parseDinkie "test/testCorrectAST.ding"
-test2 = showRoseTree $ asttorose $ correctProg $ parsetoast $ parseDinkie "test/testCorrectAST.ding"
+-- compileDinkie :: String -> [String]
+-- compileDinkie file  | length typeErrors /= 0    = error $ ('\n':) $ unlines typeErrors
+--                     | otherwise                 = []
+--                     where
+--                         parseTree = parseDinkie file
+--                         ast = parsetoast parseTree
+--                         ast' = correctProg ast
+--                         typeErrors = snd $ checkTypes [] ast'
+--
+-- test1 = showRoseTree $ asttorose $ parsetoast $ parseDinkie "test/testCorrectAST.ding"
+-- test2 = showRoseTree $ asttorose $ correctProg $ parsetoast $ parseDinkie "test/testCorrectAST.ding"
