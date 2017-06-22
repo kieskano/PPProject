166ddaa5dd3ad2589b3efefd80f8ff0369e24c07
Han Hollander <han_hollander@hotmail.nl>
2017-06-22 13:00:57 +0200

    Generator done for declarations assignments and expressions NOT TESTED

---
+++ Generator/Generator.hs
@@ -0,0 +1,100 @@
+module Generator where
+
+
+import Parser.AST.AST
+import Generator.VariableOffset
+import Sprockell
+
+
+generateCode :: AST -> (OffsetMap, OffsetMap) -> [Instruction]
+generateCode (ProgT as) vm              = generateCode' as vm
+-- Statements
+generateCode (GlobalDeclT t v a) (l,g)  = (generateCode a (l,g))++[Pop regA, WriteInstr regA (DirAddr og)]
+                                            where
+                                                og = getOffset v g
+generateCode (PrivateDeclT t v a) (l,g) = (generateCode a (l,g))++[Pop regA, Store regA (DirAddr ol)]
+                                            where
+                                                ol = getOffset v l
+generateCode (AssignT v a) (l,g)        | ol != -1      = (generateCode a (l,g))++[Pop regA, Store regA (DirAddr ol)]
+                                        | otherwise     = (generateCode a (l,g))++[Pop regA, WriteInstr regA (DirAddr og)]
+                                            where
+                                                ol = getOffset v l
+                                                og = getOffset v g
+generateCode (WhileT a as) (l,g)        = []
+generateCode (IfOneT a as) (l,g)        = []
+generateCode (IfTwoT a as1 as2) (l,g)   = []
+generateCode (ParallelT a as) (l,g)     = []
+generateCode (ReadIntT v) (l,g)         = []
+generateCode (WriteIntT a) (l,g)        = []
+-- Expressions
+generateCode EmptyT (l,g)               = [Push reg0]
+generateCode (IntConstT i) (l,g)        = [Load (ImmValue (read i)) regA, Push regA]
+generateCode (BoolConst b) (l,g)        = [Load (ImmValue (readBoolToInt b)) regA, Push regA]
+generateCode (VarT v) (l,g)             | ol != -1      = [Load (DirAddr ol) regA, Push regA]
+                                        | otherwise     = [Read (DirAddr og), Receive regA, Push regA]
+                                            where
+                                                ol = getOffset v l
+                                                og = getOffset v g
+generateCode ThreadIDT (l,g)            = [Push regSprID]
+generateCode (OneOpT o a) (l,g)         | o == "-"      = (generateCode a (l,g))++[Pop regA, Load (ImmValue -1) regB, Compute Mul regA regB regA, Push regA]
+                                        | o == "!"      = (generateCode a (l,g))++[Pop regA, Load (ImmValue 1) regB, Compute Xor regA regB regA, Push regA]
+generateCode (TwoOpT a1 o a2) (l,g)     | o == "*"      = geneateTwoOpCode a1 a2 Mul (l,g)
+                                        | o == "%"      = geneateTwoOpCode a1 a2 Div (l,g)
+                                        | o == "+"      = geneateTwoOpCode a1 a2 Add (l,g)
+                                        | o == "-"      = geneateTwoOpCode a1 a2 Sub (l,g)
+                                        | o == "=="     = geneateTwoOpCode a1 a2 Equal (l,g)
+                                        | o == ">"      = geneateTwoOpCode a1 a2 Gt (l,g)
+                                        | o == "<"      = geneateTwoOpCode a1 a2 Lt (l,g)
+                                        | o == ">="     = geneateTwoOpCode a1 a2 Gte (l,g)
+                                        | o == "<="     = geneateTwoOpCode a1 a2 Lte (l,g)
+                                        | o == "!="     = geneateTwoOpCode a1 a2 NEq (l,g)
+                                        | o == "||"     = geneateTwoOpCode a1 a2 Or (l,g)
+                                        | o == "&&"     = geneateTwoOpCode a1 a2 And (l,g)
+                                        | o == "+|"     = geneateTwoOpCode a1 a2 Xor (l,g)
+generateCode (Brackets a) (l,g)         = generateCode a (l,g)
+
+
+
+
+
+generateTwoOpCode :: AST -> AST -> Operator
+generateTwoOpCode a1 a2 o (l,g)     = (generateCode a1 (l,g))++(generateCode a2 (l,g))++[Pop regA, Pop regB, Compute o regA regB regA, Push regA]
+
+
+
+
+
+
+
+generateCode' :: [AST] -> (OffsetMap, OffsetMap) -> [Instruction]
+generateCode' [] vm                 =
+generateCode' (a:as) vm             = (generateCode a vm)++(generateCode' as vm)
+
+
+getOffset :: String -> OffsetMap -> Int
+getOffset s []          = -1
+getOffset s (o:os)      | fst o == s        = snd o
+                        | otherwise         = getOffset s os
+
+
+readBoolToInt :: String -> Int
+readBoolToInt s | s == "\\"     = 0
+                | s == "/"      = 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
