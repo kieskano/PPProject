6a7ce29f9c2098ee4452b56f46053f3905cb9f9c
Wijtse Rekker <wijtse@rekkerit.nl>
2017-07-06 10:11:12 +0200

    hotfix

--- Generator/VariableOffset.hs
+++ Generator/VariableOffset.hs
@@ -125,7 +125,7 @@ calculateThreadAmount (IfTwoT a as1 as2)        = maximum [calculateThreadAmount
 calculateThreadAmount (ParallelT s as)          = read s
 calculateThreadAmount (ReadStatT t s)           = 1
 calculateThreadAmount (WriteStatT t a)          = calculateThreadAmount a
-calculateThreadAmount (ReuturnT a)              = 1
+calculateThreadAmount (ReturnT a)              = 1
 --Expressions
 calculateThreadAmount EmptyT                    = 1
 calculateThreadAmount (IntConstT s)             = 1
